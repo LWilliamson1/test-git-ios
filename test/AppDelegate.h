7950008d2ee29169ad09edea67cb4d95754d70a7
Brittany Daniels <brittany@Brittanys-MacBook-Pro.local>
2013-01-20 20:02:47 -0500

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Brittany Daniels on 1/20/13.
+//  Copyright (c) 2013 Brittany Daniels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testTests/testTests.h
@@ -0,0 +1,13 @@
+//
+//  testTests.h
+//  testTests
+//
+//  Created by Brittany Daniels on 1/20/13.
+//  Copyright (c) 2013 Brittany Daniels. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testTests : SenTestCase
+
+@end
