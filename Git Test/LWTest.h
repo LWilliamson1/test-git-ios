b615246578d3e44816d9b2dc2a70a09a084177f0
Larry Williamson <Larry@Larrys-MacBook-Air.local>
2014-09-02 22:51:28 -0400

    Added Test class

---
+++ Git Test/LWTest.h
@@ -0,0 +1,13 @@
+//
+//  LWTest.h
+//  Git Test
+//
+//  Created by Larry Williamson on 9/2/14.
+//  Copyright (c) 2014 Larry Williamson Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LWTest : NSObject
+
+@end
---
+++ Git Test/LWTest.m
@@ -0,0 +1,13 @@
+//
+//  LWTest.m
+//  Git Test
+//
+//  Created by Larry Williamson on 9/2/14.
+//  Copyright (c) 2014 Larry Williamson Inc. All rights reserved.
+//
+
+#import "LWTest.h"
+
+@implementation LWTest
+
+@end
